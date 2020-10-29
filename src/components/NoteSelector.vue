4cfb2573c6953dcc60f9405be7e89bde2803ee13
dani <dani.zaghian@gmail.com>
2020-10-29 11:17:16 -0700

    adds props and emit for active class

--- src/components/NoteSelector.vue
+++ src/components/NoteSelector.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="note-selector">
+  <div
+    class="note-selector"
+    v-bind:class="{ active: selectedNoteId === note.id }"
+    v-on:click="selectNote(note)"
+  >
     <p class="note-selector-title">{{ note.body }}</p>
     <p class="note-selector-timestamp">{{ note.timestamp }}</p>
   </div>
@@ -7,6 +11,11 @@
 
 <script>
 export default {
-  props: ["note"],
+  props: ["note", "selectedNoteId"],
+  methods: {
+    selectNote: function(note) {
+      this.$emit("selectNote", note);
+    },
+  },
 };
 </script>
--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -3,7 +3,9 @@
     <NoteSelector
       v-for="note in notes"
       v-bind:note="note"
+      v-bind:selectedNoteId="selectedNoteId"
       v-bind:key="note.id"
+      v-on:selectNote="selectNote"
     />
   </div>
 </template>
@@ -23,7 +25,13 @@ export default {
         { id: 4, body: "This is the fourth note", timestamp: Date.now() },
         { id: 5, body: "This is the fifth note", timestamp: Date.now() },
       ],
+      selectedNoteId: 1,
     };
+  },
+  methods: {
+    selectNote: function(note) {
+      this.selectedNoteId = note.id;
+    },
   },
 };
 </script>
